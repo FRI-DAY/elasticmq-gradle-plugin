4aab3e634e185ea9abc1b42e657bc0d2db75f1fb
Rui Rosário <rpsrosario@outlook.com>
2018-08-26 23:06:52 +0200

    Make Property delegates a bit more Type safe
    Adds a Type annotation to both GradleProperty and GradleIntProperty to
    ensure a bit more type safety as well as remove the nullability of the
    delegator instance.

--- src/main/kotlin/GradleProperty.kt
+++ src/main/kotlin/GradleProperty.kt
@@ -3,24 +3,24 @@ package de.friday.gradle.elasticmq
 import kotlin.reflect.KProperty
 import org.gradle.api.Project
 
-internal class GradleProperty<T>(
+internal class GradleProperty<T, V>(
     project: Project,
-    type: Class<T>,
-    default: T? = null
+    type: Class<V>,
+    default: V? = null
 ) {
     val property = project.objects.property(type).apply {
         set(default)
     }
 
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
+    operator fun getValue(thisRef: T, property: KProperty<*>): V =
         this.property.get()
 
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) =
+    operator fun setValue(thisRef: T, property: KProperty<*>, value: V) =
         this.property.set(value)
 }
 
 @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-internal class GradleIntProperty(
+internal class GradleIntProperty<T>(
     project: Project,
     default: Int? = null
 ) {
@@ -28,9 +28,9 @@ internal class GradleIntProperty(
         set(default as? Integer)
     }
 
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): Int =
+    operator fun getValue(thisRef: T, property: KProperty<*>): Int =
             this.property.get().toInt()
 
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) =
+    operator fun setValue(thisRef: T, property: KProperty<*>, value: Int) =
             this.property.set(value as? Integer)
 }
